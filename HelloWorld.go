11a5539c3eb72cbe1af32c71a553c5d6a46a3238
Julien Legrand <julien.legrand@atos.net>
2017-07-10 11:49:09 +0200

    refactor + const

--- HelloWorld.go
+++ HelloWorld.go
@@ -6,6 +6,13 @@ import (
 )
 
 func main() {
+
+	defvar()
+
+}
+
+func defvar() {
+
 	var x float64
 	x = 20.0
 	fmt.Println(x)
@@ -22,4 +29,15 @@ func main() {
 	fmt.Printf("char is of type %T \n", char)
 	fmt.Printf("%d \n", char)
 	fmt.Printf("%q \n", char)
+
+	e := 9000.7
+	fmt.Printf("e is of type %T \n", e)
+
+	var a, b, c = 1, 8.0, "etet"
+	fmt.Printf("a is of type %T \n", a)
+	fmt.Printf("b is of type %T \n", b)
+	fmt.Printf("c is of type %T \n", c)
+
+	const LENGTH int = 10
+
 }
