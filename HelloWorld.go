116ad593aa0583ccd15264bcae8b3eceed644142
Julien Legrand <julien.legrand@atos.net>
2017-07-10 11:41:09 +0200

    variables and type

--- HelloWorld.go
+++ HelloWorld.go
@@ -6,5 +6,20 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	var x float64
+	x = 20.0
+	fmt.Println(x)
+	fmt.Printf("x is of type %T \n", x)
+	fmt.Printf("x value is %f \n", x)
+
+	var chaine string
+	chaine = "ma chaine en utf-8"
+	fmt.Println(chaine)
+	fmt.Printf("%s \n", chaine)
+
+	var char rune
+	char = 'r'
+	fmt.Printf("char is of type %T \n", char)
+	fmt.Printf("%d \n", char)
+	fmt.Printf("%q \n", char)
 }
