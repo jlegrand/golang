a21223869e1abaf9e0fd5cb0c0e5a871be110e6c
Julien Legrand <julien.legrand@atos.net>
2017-07-11 09:36:23 +0200

    eclatement de tableau

--- HelloWorld.go
+++ HelloWorld.go
@@ -6,7 +6,8 @@ import (
 	//"github.com/jlegrand/golang/boucles"
 	//"github.com/jlegrand/golang/variables"
 	//"github.com/jlegrand/golang/pointers"
-	"github.com/jlegrand/golang/fonction"
+	//"github.com/jlegrand/golang/fonction"
+	"fmt"
 )
 
 func main() {
@@ -23,11 +24,17 @@ func main() {
 	//fonction.Anonymous()
 	//fmt.Println(fonction.Calcul('/', 12, 5))
 	//fonction.Closure()
-	fonction.FonctionVariadique(1)
+
+	/*fonction.FonctionVariadique(1)
 	fonction.FonctionVariadique(1, 2)
 	fonction.FonctionVariadique(1,2,3)
 	// éclatement de la structure avec ...
-	fonction.FonctionVariadique([]int{9,8,7,6,5,4,3,2,1}...)
+	fonction.FonctionVariadique([]int{9,8,7,6,5,4,3,2,1}...)*/
+
+	var t1 = []int{1,2,3}
+	var t2 = []int{4,5,6}
+	var t []int = append(t1,t2...)
+	fmt.Println(t)
 
 	//pointers.Swap(10, 5)
 
